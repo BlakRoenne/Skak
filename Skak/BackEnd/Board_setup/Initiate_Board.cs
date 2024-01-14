40366bfa68806beaad12690cab412f73a6e878f0
BlakRoenne <94530885+BlakRoenne@users.noreply.github.com>
2024-01-14 19:11:13 +0100

    Påbegyndt mindre dele af troops

--- Skak/BackEnd/Board_setup/Initiate_Board.cs
+++ Skak/BackEnd/Board_setup/Initiate_Board.cs
@@ -1,23 +1,35 @@
-﻿namespace Skak.BackEnd.Board_setup
+﻿using Skak.BackEnd.Troops;
+
+namespace Skak.BackEnd.Board_setup
 {
-    public class InitiateBoard()
+
+    public class InitiateBoard
     {
-        public void boardSize() {
+
+        public void boardSize()
+        {
             //variables
-        int height = 8;
-        int width = 8;
-        //a tiny bit of caluclation
-        for (int i = 0; i<height; i++)//looper igennem rækker
+            int height = 8;
+            int width = 8;
+            //a tiny bit of caluclation
+            for (int i = 0; i < height; i++)//looper igennem rækker
                 for (int j = 0; j < width; j++)//looper igennem kolonner
-                    if ((i+j)%2==0)
+                    if ((i + j) % 2 == 0)
                     {
                         Console.Write("X"); //white square
                     }
                     else
                         Console.Write("0"); //black square
-                 Console.WriteLine();//New line 
+            Console.WriteLine();//New line 
         }
-        
+        public void boardSize()
+        {
+            // Existing board setup logic
+
+            // Example: Assume you have a shared context or method
+            SharedContext.SetBoardState(/* pass the necessary parameters */);
+        }
     }
 
 }
+
